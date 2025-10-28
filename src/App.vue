fe612a64abfba6610292551179805a18705269b7
danli <ingnaane@hotmail.com>
2025-10-28 12:00:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+ export default {
+    data() {
+      return {
+        // api_key: `http://api.weatherapi.com/v1/current.json?key=3e8306ca69624ad9b24134306252010&q=${city}`
+    
+      };
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+
+<template>
+  <h1>Weather info 🌤️</h1>
+  <div id="app">
+    <main>
+      <div class="searh-box">
+        <input type="text" class="search-box" placeholder="Write a city">
+        <button id="getWeatherBtn">Search</button>
+      </div>
+    </main>
+    
+  </div>
+  
+</template>
+
+<style scoped>
+#app {
+  background-image: url();
+  background-size: cover;
+}
+
+</style>
